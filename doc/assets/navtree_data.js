65ee447cadfef04343b62406c0c0c9d6a98b09f8
allentuh <allentcm@gmail.com>
2016-02-15 11:29:43 +0800

    Javadoc v0.1.0

---
+++ doc/assets/navtree_data.js
@@ -0,0 +1,17 @@
+var NAVTREE_DATA =
+[ [ "com.he5ed.lib.cloudprovider", "com/he5ed/lib/cloudprovider/package-summary.html", [ [ "Interfaces", null, [ [ "CloudProvider.OnAccountChangeListener", "com/he5ed/lib/cloudprovider/CloudProvider.OnAccountChangeListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "CloudProvider", "com/he5ed/lib/cloudprovider/CloudProvider.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.he5ed.lib.cloudprovider.apis", "com/he5ed/lib/cloudprovider/apis/package-summary.html", [ [ "Interfaces", null, [ [ "BaseApi.OnPrepareListener", "com/he5ed/lib/cloudprovider/apis/BaseApi.OnPrepareListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "BaseApi", "com/he5ed/lib/cloudprovider/apis/BaseApi.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.he5ed.lib.cloudprovider.exceptions", "com/he5ed/lib/cloudprovider/exceptions/package-summary.html", [ [ "Exceptions", null, [ [ "AuthFailException", "com/he5ed/lib/cloudprovider/exceptions/AuthFailException.html", null, "" ], [ "ConflictException", "com/he5ed/lib/cloudprovider/exceptions/ConflictException.html", null, "" ], [ "InvalidUrlException", "com/he5ed/lib/cloudprovider/exceptions/InvalidUrlException.html", null, "" ], [ "RequestFailException", "com/he5ed/lib/cloudprovider/exceptions/RequestFailException.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.he5ed.lib.cloudprovider.models", "com/he5ed/lib/cloudprovider/models/package-summary.html", [ [ "Classes", null, [ [ "CFile", "com/he5ed/lib/cloudprovider/models/CFile.html", null, "" ], [ "CFolder", "com/he5ed/lib/cloudprovider/models/CFolder.html", null, "" ], [ "CloudAccount", "com/he5ed/lib/cloudprovider/models/CloudAccount.html", null, "" ], [ "User", "com/he5ed/lib/cloudprovider/models/User.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.he5ed.lib.cloudprovider.picker", "com/he5ed/lib/cloudprovider/picker/package-summary.html", [ [ "Classes", null, [ [ "CloudPickerActivity", "com/he5ed/lib/cloudprovider/picker/CloudPickerActivity.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
